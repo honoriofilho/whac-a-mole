40ecf7b17f20d9ff5afb86ffed89a54e1b4b0957
honoriofilho <honoriovitor.web@gmail.com>
2024-11-08 11:43:40 -0300

    animacao feita

--- SpatialComputing/ContentView.swift
+++ SpatialComputing/ContentView.swift
@@ -10,20 +10,50 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+        
     var body: some View {
         VStack {
-            Model3D(named: "Topeira", bundle: realityKitContentBundle)
-            Text("Hello, world!")
+            RealityView { content in
+                if let entity = try? await Entity(named: "buraco_pinguim", in: realityKitContentBundle) {
+                    for anim in entity.availableAnimations {
+                        entity.playAnimation(anim.repeat(duration: .infinity),
+                                             transitionDuration: 1.25,startsPaused: true)
+                    }
+                    content.add(entity)
+                } else {
+                    print("Erro ao carregar a entidade ou animação")
+                }
+            }
         }
         .gesture(TapGesture().targetedToAnyEntity()
             .onEnded({ value in
                 _ = value.entity.applyTapForBehaviors()
             })
         )
-        //.padding()
+    }
+    
+    
+    // MARK: Helpers
+    private func getTopeiraIfAvailable(scene: RealityFoundation.Scene) -> Entity? {
+        if let topeira = scene.findEntity(named: "Topeira"),
+           let topeiraComponent = topeira.components[TopeiraRuntimeComponent.self],
+           topeiraComponent.isAvailable() {
+            return topeira
+        }
+
+        return nil
+    }
+
+    private func getTopeira(from scene: RealityFoundation.Scene?) -> Entity? {
+        guard let topeira = scene?.findEntity(named: "Topeira"),
+              topeira.components[TopeiraRuntimeComponent.self] != nil else {
+            return nil
+        }
+
+        return topeira
     }
 }
 
-#Preview(windowStyle: .plain) {
+#Preview(windowStyle: .volumetric) {
     ContentView()
 }
